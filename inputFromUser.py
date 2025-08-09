09206639a20cb66122de0d4e4857f3fd5025dc7c
vastavikadi <aditya82004shukla@gmail.com>
2025-08-10 01:59:25 +0530

    input from user

---
+++ inputFromUser.py
@@ -0,0 +1,3 @@
+# this is how we take inputs from the users in python
+a = input('Enter your name: ')
+print(a)
---
+++ typeCasting.py
@@ -0,0 +1,8 @@
+# This is how we check the type of the variable in Python
+a=31
+print(type(a)) #this is the type of integer
+
+
+# to explicitly convert the variable to a different type, we can use the built-in functions like int(), float(), and str().
+t = str(a)
+print(type (t))
