3ebced33f994c9df6e5517629374afcd14291933
vastavikadi <aditya82004shukla@gmail.com>
2025-08-10 02:02:50 +0530

    additional input info

--- inputFromUser.py
+++ inputFromUser.py
@@ -1,3 +1,13 @@
 # this is how we take inputs from the users in python
 a = input('Enter your name: ')
 print(a)
+
+# This by default takes the input as a string and we can convert it to other types if needed.
+
+b = input('Enter a number to add to another number: ')
+c = input("Enter a number to add to the first number: ")
+
+b = int(b)
+c = int (c)
+
+print('The sum is: ',b+c)
