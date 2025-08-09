1fa6ff2eadec4e2c02e0f7993e4cd8598b6d71b1
vastavikadi <aditya82004shukla@gmail.com>
2025-08-10 01:35:52 +0530

    datatypes in python

---
+++ datatypes.py
@@ -0,0 +1,16 @@
+# a=1 a is a variables and 1 that is the value being assigned to a is a literal and that is what a is storing in the memory somewhere and we can call 'a' an identifier as well.
+
+# There are the following dataypes in python:
+# Integers 5
+# Floating point numbers 5.55
+# Strings "Hello, World!"
+# Booleans True/False
+# NoneType None (means no value, particular variable has no value)
+
+# Rules for defining variables in python:
+#     1. Variable names can contain letters, numbers, and underscores.
+#     2. Variable names must start with a letter or an underscore.
+#     3. Variable names are case-sensitive (e.g., 'myVar' and 'myvar' are different variables).
+#     4. Variable names cannot be the same as Python keywords (e.g., 'def', 'class', 'if', etc.).
+
+
